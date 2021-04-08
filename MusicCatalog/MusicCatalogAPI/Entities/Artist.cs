77f54807ac9d4ed14e52f46fcdd6ae77677f0c84
Marek Sroczkowski <marekes97@gmail.com>
2021-04-08 13:20:43 +0200

    Created entity class for artist

---
+++ MusicCatalog/MusicCatalogAPI/Entities/Artist.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MusicCatalogAPI.Entities
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Album> Albums { get; set; }
+    }
+}
