0c1075d9fe1231def23b5077ca20ec61d9fe9b99
Marek Sroczkowski <marekes97@gmail.com>
2021-04-08 13:20:07 +0200

    Created entity class for music supplier

---
+++ MusicCatalog/MusicCatalogAPI/Entities/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MusicCatalogAPI.Entities
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+
+        public virtual List<Album> Albums { get; set; }
+    }
+}
