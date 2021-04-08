121b4993377c12276423cf7251c0d03fa307de94
Marek Sroczkowski <marekes97@gmail.com>
2021-04-08 13:21:26 +0200

    Created entity class for song

---
+++ MusicCatalog/MusicCatalogAPI/Entities/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicCatalogAPI.Entities
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PublicationYear { get; set; }
+        public double Duration { get; set; }
+
+        public virtual Album Album { get; set; }
+        public int AlbumId { get; set; }
+    }
+}
