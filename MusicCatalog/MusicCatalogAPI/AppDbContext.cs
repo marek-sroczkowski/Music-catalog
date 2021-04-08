fb06b2da08da96af22948be369c2b824db65cbbc
Marek Sroczkowski <marekes97@gmail.com>
2021-04-08 14:50:23 +0200

    Added db context class

---
+++ MusicCatalog/MusicCatalogAPI/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicCatalogAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCatalogAPI
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=MusicCatalogDb;Trusted_Connection=True;";
+
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
