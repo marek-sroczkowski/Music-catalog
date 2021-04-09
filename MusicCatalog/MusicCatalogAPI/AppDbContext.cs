5896a3d220990ba7b6cd482b95e95a6b1dc49472
Marek Sroczkowski <marekes97@gmail.com>
2021-04-09 23:52:22 +0200

    Added seeder with simple data

--- MusicCatalog/MusicCatalogAPI/AppDbContext.cs
+++ MusicCatalog/MusicCatalogAPI/AppDbContext.cs
@@ -11,6 +11,7 @@ namespace MusicCatalogAPI
     {
         private readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=MusicCatalogDb;Trusted_Connection=True;";
 
+        public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Supplier> Suppliers { get; set; }
         public virtual DbSet<Album> Albums { get; set; }
         public virtual DbSet<Artist> Artists { get; set; }
@@ -18,7 +19,8 @@ namespace MusicCatalogAPI
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Supplier>().ToTable("Suppliers");
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- MusicCatalog/MusicCatalogAPI/Startup.cs
+++ MusicCatalog/MusicCatalogAPI/Startup.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MusicCatalogAPI.Entities;
 
 namespace MusicCatalogAPI
 {
@@ -25,11 +27,14 @@ namespace MusicCatalogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddControllers();
+            services.AddDbContext<AppDbContext>();
+            services.AddScoped<SeedData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedData seeder)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +51,8 @@ namespace MusicCatalogAPI
             {
                 endpoints.MapControllers();
             });
+
+            seeder.Seed();
         }
     }
 }
