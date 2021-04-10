968848488bac04bfd885b2584dae4e578a7d5b4a
Marek Sroczkowski <marekes97@gmail.com>
2021-04-10 11:59:13 +0200

    Added application mapping profile

---
+++ MusicCatalog/MusicCatalogAPI/Mapping/MusicCatalogProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MusicCatalogAPI.Entities;
+using MusicCatalogAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCatalogAPI.Mapping
+{
+    public class MusicCatalogProfile : Profile
+    {
+        public MusicCatalogProfile()
+        {
+            CreateMap<RegisterSupplierDto, Supplier>()
+                .ReverseMap();
+        }
+    }
+}
