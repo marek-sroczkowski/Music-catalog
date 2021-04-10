ae5910a02d3e09f84ad3f9eca98760a6397959e6
Marek Sroczkowski <marekes97@gmail.com>
2021-04-10 11:57:02 +0200

    Added dtos for login and supplier's registration

---
+++ MusicCatalog/MusicCatalogAPI/Models/RegisterSupplierDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCatalogAPI.Models
+{
+    public class RegisterSupplierDto
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
