982732d017035d1c0123b16c086916829504039a
Marek Sroczkowski <marekes97@gmail.com>
2021-04-12 15:45:08 +0200

    Created dto for album

---
+++ MusicCatalog/MusicCatalogAPI/Models/AlbumDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCatalogAPI.Models
+{
+    public class AlbumDto
+    {
+        public int Id { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+        public int PublicationYear { get; set; }
+        public string Supplier { get; set; }
+    }
+}
