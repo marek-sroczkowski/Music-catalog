634e5070d0c6f480b720fd221d1b57780ece5f80
Marek Sroczkowski <marekes97@gmail.com>
2021-04-15 11:20:57 +0200

    Added method for update album

--- MusicCatalog/MusicCatalogAPI/Repositories/AlbumRepository.cs
+++ MusicCatalog/MusicCatalogAPI/Repositories/AlbumRepository.cs
@@ -11,11 +11,13 @@ namespace MusicCatalogAPI.Repositories
     {
         private readonly AppDbContext dbContext;
         private readonly IUserRepository userRepo;
+        private readonly IArtistRepository artistRepo;
 
-        public AlbumRepository(AppDbContext dbContext, IUserRepository userRepo)
+        public AlbumRepository(AppDbContext dbContext, IUserRepository userRepo, IArtistRepository artistRepo)
         {
             this.dbContext = dbContext;
             this.userRepo = userRepo;
+            this.artistRepo = artistRepo;
         }
 
         public async Task<ICollection<Album>> GetAlbumsAsync(string username)
@@ -36,20 +38,32 @@ namespace MusicCatalogAPI.Repositories
             .Include(a => a.Artist)
             .FirstOrDefaultAsync(a => a.Id.Equals(albumId));
 
-        public async Task AddAlbum(string username, Album album)
+        public async Task AddAlbumAsync(string username, Album album)
         {
             var supplier = await userRepo.GetSupplierAsync(username);
+            var artist = await artistRepo.GetArtistAsync(album.Artist.Name);
+
+            if (artist != null)
+                album.Artist = artist;
 
             supplier?.Albums.Add(album);
             await dbContext.SaveChangesAsync();
         }
 
-        public async Task UpdateAlbum(int albumId, Album newAlbum)
+        public async Task UpdateAlbumAsync(int albumId, Album newAlbum)
         {
+            var album = await GetAlbumAsync(albumId);
 
+            album.Artist.Name = newAlbum.Artist.Name;
+            album.PublicationYear = newAlbum.PublicationYear;
+            album.Title = newAlbum.Title;
+            album.Version = newAlbum.Version;
+
+            dbContext.Albums.Update(album);
+            await dbContext.SaveChangesAsync();
         }
 
-        public async Task DeleteAlbum(int albumId)
+        public async Task DeleteAlbumAsync(int albumId)
         {
             var album = await GetAlbumAsync(albumId);
 
--- MusicCatalog/MusicCatalogAPI/Repositories/IAlbumRepository.cs
+++ MusicCatalog/MusicCatalogAPI/Repositories/IAlbumRepository.cs
@@ -6,10 +6,10 @@ namespace MusicCatalogAPI.Repositories
 {
     public interface IAlbumRepository
     {
-        Task AddAlbum(string username, Album album);
-        Task DeleteAlbum(int albumId);
+        Task AddAlbumAsync(string username, Album album);
+        Task DeleteAlbumAsync(int albumId);
         Task<Album> GetAlbumAsync(int albumId);
         Task<ICollection<Album>> GetAlbumsAsync(string username);
-        Task UpdateAlbum(int albumId, Album newAlbum);
+        Task UpdateAlbumAsync(int albumId, Album newAlbum);
     }
 }
