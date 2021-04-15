21c324be1d99a64ccb9f4a440e3bd30453cdb5c3
Marek Sroczkowski <marekes97@gmail.com>
2021-04-15 11:16:16 +0200

    Created repository class for artist

---
+++ MusicCatalog/MusicCatalogAPI/Repositories/ArtistRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicCatalogAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCatalogAPI.Repositories
+{
+    public class ArtistRepository : IArtistRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public ArtistRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<ICollection<Artist>> GetArtistsAsync() => await dbContext.Artists
+            .Include(a => a.Albums)
+            .ToListAsync();
+
+        public async Task<Artist> GetArtistAsync(string name) => await dbContext.Artists
+            .Include(a => a.Albums)
+            .FirstOrDefaultAsync(a => a.Name.Equals(name));
+    }
+}
---
+++ MusicCatalog/MusicCatalogAPI/Repositories/IArtistRepository.cs
@@ -0,0 +1,12 @@
+﻿using MusicCatalogAPI.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicCatalogAPI.Repositories
+{
+    public interface IArtistRepository
+    {
+        Task<Artist> GetArtistAsync(string name);
+        Task<ICollection<Artist>> GetArtistsAsync();
+    }
+}
