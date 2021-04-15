fd6364661b1e951eb1ebe45c0d5c392b115738bb
Marek Sroczkowski <marekes97@gmail.com>
2021-04-15 11:22:21 +0200

    Added HttpPost, HttpPut, HttpDelete request for endpoint /api/album

--- MusicCatalog/MusicCatalogAPI/Controllers/AlbumController.cs
+++ MusicCatalog/MusicCatalogAPI/Controllers/AlbumController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MusicCatalogAPI.Authorization;
+using MusicCatalogAPI.Entities;
 using MusicCatalogAPI.Models;
 using MusicCatalogAPI.Repositories;
 using System;
@@ -15,15 +17,15 @@ namespace MusicCatalogAPI.Controllers
     [Authorize]
     public class AlbumController : ControllerBase
     {
-        private readonly IUserRepository userRepo;
         private readonly IAlbumRepository albumRepo;
         private readonly IMapper mapper;
+        private readonly IAuthorizationService authorizationService;
 
-        public AlbumController(IUserRepository userRepo, IAlbumRepository albumRepo, IMapper mapper)
+        public AlbumController(IAlbumRepository albumRepo, IMapper mapper, IAuthorizationService authorizationService)
         {
-            this.userRepo = userRepo;
             this.albumRepo = albumRepo;
             this.mapper = mapper;
+            this.authorizationService = authorizationService;
         }
 
         [HttpGet]
@@ -42,12 +44,68 @@ namespace MusicCatalogAPI.Controllers
         [Authorize(Roles = "MusicSupplier")]
         public async Task<ActionResult<AlbumDto>> Get(int albumId)
         {
-            var username = User.FindFirst(c => c.Type == ClaimTypes.Name).Value;
-
             var album = await albumRepo.GetAlbumAsync(albumId);
+            if (album == null)
+                return NotFound();
+
+            var authorizationResult = authorizationService.AuthorizeAsync(User, album, new ResourceOperationRequirement(OperationType.Read)).Result;
+            if (!authorizationResult.Succeeded)
+                return Forbid();
+
             var albumDto = mapper.Map<AlbumDetailsDto>(album);
 
             return Ok(albumDto);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "MusicSupplier")]
+        public async Task<ActionResult> Post([FromBody] AlbumDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var album = mapper.Map<Album>(model);
+            var username = User.FindFirst(c => c.Type == ClaimTypes.Name).Value;
+
+            await albumRepo.AddAlbumAsync(username, album);
+
+            return Created("api/album/" + album.Id, null);
+        }
+
+        [HttpPut("{albumId}")]
+        public async Task<ActionResult> Put(int albumId, [FromBody] AlbumDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var album = await albumRepo.GetAlbumAsync(albumId);
+            if (album == null)
+                return NotFound();
+
+            var authorizationResult = authorizationService.AuthorizeAsync(User, album, new ResourceOperationRequirement(OperationType.Update)).Result;
+            if (!authorizationResult.Succeeded)
+                return Forbid();
+
+            var updatedAlbum = mapper.Map<Album>(model);
+
+            await albumRepo.UpdateAlbumAsync(albumId, updatedAlbum);
+            return NoContent(); 
+        }
+
+
+        [HttpDelete("{albumId}")]
+        public async Task<ActionResult> Delete(int albumId)
+        {
+            var album = await albumRepo.GetAlbumAsync(albumId);
+            if (album == null)
+                return NotFound();
+
+            var authorizationResult = authorizationService.AuthorizeAsync(User, album, new ResourceOperationRequirement(OperationType.Delete)).Result;
+            if (!authorizationResult.Succeeded)
+                return Forbid();
+
+            await albumRepo.DeleteAlbumAsync(albumId);
+            return NoContent();
         }
     }
 }
