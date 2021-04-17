341437a2bdd7b704066152bc2ed1467ddca49a15
Marek Sroczkowski <marekes97@gmail.com>
2021-04-17 15:27:39 +0200

    Added song existence validation filter attribute

---
+++ MusicCatalog/MusicCatalogAPI/Filters/ValidateSongExistenceAttribute.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MusicCatalogAPI.Repositories;
+using System.Threading.Tasks;
+
+namespace MusicCatalogAPI.Filters
+{
+    public class ValidateSongExistenceAttribute : TypeFilterAttribute
+    {
+        public ValidateSongExistenceAttribute() : base(typeof(ValidateSongExistenceFilterImpl))
+        {
+        }
+
+        private class ValidateSongExistenceFilterImpl : IAsyncActionFilter
+        {
+            private readonly ISongRepository songRepo;
+
+            public ValidateSongExistenceFilterImpl(ISongRepository songRepo)
+            {
+                this.songRepo = songRepo;
+            }
+
+            public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+            {
+                if (context.ActionArguments.ContainsKey("albumId") && context.ActionArguments.ContainsKey("songId"))
+                {
+                    var albumId = context.ActionArguments["albumId"] as int?;
+                    var songId = context.ActionArguments["songId"] as int?;
+
+                    if (albumId.HasValue && songId.HasValue)
+                    {
+                        if ((await songRepo.GetSongAsync(albumId.Value, songId.Value)) == null)
+                        {
+                            context.Result = new NotFoundObjectResult(songId.Value);
+                            return;
+                        }
+                    }
+                }
+                await next();
+            }
+        }
+    }
+}
