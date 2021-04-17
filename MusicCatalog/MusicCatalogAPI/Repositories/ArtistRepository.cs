77216d04677eb47816e96120d87b94cd18cea297
Marek Sroczkowski <marekes97@gmail.com>
2021-04-17 15:36:13 +0200

    Minor code refactoring

--- MusicCatalog/MusicCatalogAPI/Repositories/ArtistRepository.cs
+++ MusicCatalog/MusicCatalogAPI/Repositories/ArtistRepository.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MusicCatalogAPI.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicCatalogAPI.Repositories
