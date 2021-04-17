8084baf2ad140d5250bf31240d587a32af29e0c1
Marek Sroczkowski <marekes97@gmail.com>
2021-04-17 15:28:31 +0200

    Added Id property to SongDto

--- MusicCatalog/MusicCatalogAPI/Models/SongDto.cs
+++ MusicCatalog/MusicCatalogAPI/Models/SongDto.cs
@@ -1,9 +1,18 @@
-﻿namespace MusicCatalogAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicCatalogAPI.Models
 {
     public class SongDto
     {
+        public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public int PublicationYear { get; set; }
+
+        [Required]
         public double Duration { get; set; }
     }
 }
