8092e0b5ac9286d6de6b5d4033776a1708a57bfd
Marek Sroczkowski <marekes97@gmail.com>
2021-04-26 16:36:42 +0200

    Added routing

---
+++ MusicCatalog/MusicCatalogAngular/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AlbumsComponent } from './albums/albums.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: NavbarComponent
+  },
+  {
+    path: 'login', 
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'albums',
+    component: AlbumsComponent
+  }
+];
