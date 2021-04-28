ebb7e828f4c8ed613110c8f0d7345e6755ba7d17
Marek Sroczkowski <marekes97@gmail.com>
2021-04-28 15:58:39 +0200

    Changed color of the navbar

--- MusicCatalog/MusicCatalogAngular/src/app/navbar/navbar.component.ts
+++ MusicCatalog/MusicCatalogAngular/src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../user.service';
+import { LoginModel, UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-navbar',
