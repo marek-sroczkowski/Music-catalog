9b82d7ed76ccfadb7a70c7f7e06833f22ed4c102
Marek Sroczkowski <marekes97@gmail.com>
2021-05-08 18:03:24 +0200

    Added mapping profile for LoginUserDto

--- MusicCatalog/MusicCatalogAPI/Models/AccountDtos/LoginUserDto.cs
+++ MusicCatalog/MusicCatalogAPI/Models/AccountDtos/LoginUserDto.cs
@@ -1,8 +1,17 @@
-﻿namespace MusicCatalogAPI.Models.AccountDtos
+﻿using AutoMapper;
+using MusicCatalogAPI.Entities;
+using MusicCatalogAPI.Mapping;
+
+namespace MusicCatalogAPI.Models.AccountDtos
 {
-    public class LoginUserDto
+    public class LoginUserDto : IMap
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<LoginUserDto, User>();
+        }
     }
 }
